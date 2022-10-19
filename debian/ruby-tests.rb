b0a976565d3ca04c8332278da7eb637115c65b11
Deepin Developer <deepin-dev@deepin.org>
2022-10-19 09:56:30 +0800

    feat: Init commit
    init ruby-rspec
    
    Log:

---
+++ debian/ruby-tests.rb
@@ -0,0 +1,82 @@
+require 'fileutils'
+
+$AUTOPKGTEST = ENV.include?("AUTOPKGTEST_TMP")
+
+def banner(title, c = '∙')
+  puts
+  puts c * (title.length + 4)
+  puts "%s %s %s" % [c, title, c]
+  puts c * (title.length + 4)
+  puts
+end
+
+def maybe_move_away(path)
+  if $AUTOPKGTEST
+    FileUtils::Verbose.mv(path, path + '.off')
+    begin
+      yield
+    ensure
+      FileUtils::Verbose.mv(path + '.off', path)
+    end
+  else
+    yield
+  end
+end
+
+def run(cmd)
+  puts(cmd)
+  maybe_move_away('lib') do
+    system(cmd)
+  end
+end
+
+arch = `dpkg --print-architecture`.strip
+
+LIBS = %w{rspec-core rspec-expectations rspec-mocks rspec-support}
+
+if $AUTOPKGTEST
+  excludes_pattern = "debian/tests/excludes/{all,autopkgtest,%s}" % arch
+else
+  excludes_pattern = "debian/tests/excludes/{all,%s}" % arch
+
+  ENV["PATH"] = [File.expand_path("rspec-core/exe"), ENV["PATH"]].join(':')
+  puts "PATH=#{ENV['PATH']}"
+  #  ________________________________________________________________________
+  # /                                                                        \
+  # | ALL THIS CAN BE REMOVED AFTER THE PREVIOUS VERSION OF RSPEC USING THE  |
+  # | VENDOR LAYOUT IS NO LONGER THERE.                                      |
+  # |                                                                        |
+  rubylib = ENV.fetch('RUBYLIB', 'debian')
+  LIBS.each do |lib|
+    rubylib += ":" + File.expand_path(File.join(lib, 'lib'))
+  end
+  puts "RUBYLIB=#{rubylib}"
+  ENV['RUBYLIB'] = rubylib
+  # |                                                                        |
+  # \________________________________________________________________________/
+
+
+
+end
+
+ENV['NO_COVERAGE'] = 'yes'
+
+failed = []
+LIBS.each do |lib|
+  excludes = []
+  Dir.glob(File.join(excludes_pattern, lib)).each do |f|
+    excludes += File.readlines(f).map(&:strip)
+  end
+  banner lib
+
+  FileUtils::Verbose.chdir(lib) do
+    files = Dir['spec/**/*_spec.rb'] - excludes
+    testcmd = '%s -S rspec %s' % [RbConfig.ruby, files.join(' ')]
+    failed << lib unless run(testcmd)
+  end
+end
+
+unless failed.empty?
+  puts "Failed: %s" % failed.join(', ')
+  exit(1)
+end
---
+++ rspec-support/spec/rspec/support/reentrant_mutex_spec.rb
@@ -0,0 +1,54 @@
+require 'rspec/support/reentrant_mutex'
+require 'thread_order'
+
+# There are no assertions specifically
+# They are pass if they don't deadlock
+RSpec.describe RSpec::Support::ReentrantMutex do
+  let!(:mutex) { described_class.new }
+  let!(:order) { ThreadOrder.new }
+  after { order.apocalypse! }
+
+  it 'can repeatedly synchronize within the same thread' do
+    mutex.synchronize { mutex.synchronize { } }
+  end
+
+  it 'locks other threads out while in the synchronize block' do
+    order.declare(:before) { mutex.synchronize { } }
+    order.declare(:within) { mutex.synchronize { } }
+    order.declare(:after)  { mutex.synchronize { } }
+
+    order.pass_to :before, :resume_on => :exit
+    mutex.synchronize { order.pass_to :within, :resume_on => :sleep }
+    order.pass_to :after, :resume_on => :exit
+  end
+
+  it 'resumes the next thread once all its synchronize blocks have completed' do
+    order.declare(:thread) { mutex.synchronize { } }
+    mutex.synchronize { order.pass_to :thread, :resume_on => :sleep }
+    order.join_all
+  end
+
+  if RUBY_VERSION >= '3.0'
+    it 'waits when trying to lock from another Fiber' do
+      mutex.synchronize do
+        ready = false
+        f = Fiber.new do
+          expect {
+            ready = true
+            mutex.send(:enter)
+            raise 'should reach here: mutex is already locked on different Fiber'
+          }.to raise_error(Exception, 'waited correctly')
+        end
+
+        main_thread = Thread.current
+
+        t = Thread.new do
+          Thread.pass until ready && main_thread.stop?
+          main_thread.raise Exception, 'waited correctly'
+        end
+        f.resume
+        t.join
+      end
+    end
+  end
+end
